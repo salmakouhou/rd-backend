bc9b995359f5a571db3e01cbb7ad9162fbd2a3b5
salmakouhou <59792796+salmakouhou@users.noreply.github.com>
2021-05-26 10:48:13 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ config  = {
     MONGODB_URI : process.env.MONGODB_URI,
     EMAIL_ADDRESS: process.env.EMAIL_ADDRESS,
     EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
-    APPLICATION_URL: "https://rs-client-seven.vercel.app/"
+    APPLICATION_URL: "https://lti-master-1-git-main-salmakouhou.vercel.app/"
 }
 
 module.exports = config;
